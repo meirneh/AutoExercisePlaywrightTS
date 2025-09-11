94c71b26e9de41b6e4bbd84b19c327ea9e0ed8d4
Meir Nehemkin <meirnehemkin@gmail.com>
2025-09-11 07:26:37 +0300

    create TC-02 in spec user4 negative

--- tests/api-tests/user-negative-api-test.spec.ts
+++ tests/api-tests/user-negative-api-test.spec.ts
@@ -54,20 +54,37 @@ test.describe('User API - negative (create/delete)', () => {
         expect(json.message).toEqual("User created!");
     });
 
-    test('TC - 02 Verify Login ', async ({ request }) => {
-        const res = await request.post('https://automationexercise.com/api/verifyLogin', {
+    test('TC - 02 Create/Register User Account with registered email (negative) ', async ({ request }) => {
+        const form = new URLSearchParams({
+            name: USER_NAME,
+            email: USER_EMAIL,
+            password: USER_PASSWORD,
+            title: "Mr",
+            birth_date: "11",
+            birth_month: "12",
+            birth_year: "1975",
+            firstname: "Haim",
+            lastname: "Cohen",
+            company: "Cohen.td",
+            address1: "Ha Iasmin 10",
+            address2: "Ha Zayt 20",
+            country: "Israel",
+            state: "Central District",
+            city: "Tel Aviv",
+            zipcode: "12345",
+            mobile_number: "050-1234567"
+        }).toString();
+
+        const res = await request.post('https://automationexercise.com/api/createAccount', {
             headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-            data: new URLSearchParams({
-                email: USER_EMAIL,
-                password: USER_PASSWORD,
-            }).toString(),
+            data: form,
         });
         expect(res.status(), 'status code').toBe(200);
         const bodyText = await res.text();
         const json = safeParse(bodyText);
         console.log('RESPONSE SCHEMA:\n', JSON.stringify(json, null, 2));
-        expect(json.responseCode).toEqual(200);
-        expect(json.message).toEqual("User exists!");
+        expect(json.responseCode).toEqual(400);
+        expect(json.message).toEqual("Email already exists!");
     });
 
 
